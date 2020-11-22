0323f532cb48ade84d9dd02a40e253988672a345
kintaro1221 <74291797+kintaro1221@users.noreply.github.com>
2020-11-22 20:14:56 +0900

    ssss ssss

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ColorfulMessage from "./components/ColorfulMessage";
 
 const App = () => {
   const [num, setNum] = useState(0);
-  const [faceShowFlag, setFaceShowFlag] = useState(true);
+  const [faceShowFlag, setFaceShowFlag] = useState(false);
 
   const onClickCountUp = () => {
     setNum(num + 1);
@@ -11,6 +11,16 @@ const App = () => {
   const onClickShowFlag = () => {
     setFaceShowFlag(!faceShowFlag);
   };
+  useEffect(() => {
+    if (num > 0) {
+      if (num % 3 === 0) {
+        faceShowFlag || setFaceShowFlag(true);
+      } else {
+        faceShowFlag && setFaceShowFlag(false);
+      }
+    }
+    // eslint-disable-next-line
+  }, [num]);
 
   return (
     <>
